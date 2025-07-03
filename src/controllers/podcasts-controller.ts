7c514b70d0d2c03f032944dbc8508275244bd944
Thallentos <thallescmarques@gmail.com>
2025-07-03 06:13:09 -0300

    fazendo uma requisição do tipo get

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,1 +1,15 @@
 import { IncomingMessage, ServerResponse } from "http";
+
+export const getListEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+  res.end(
+    JSON.stringify({
+      name: "thallentos é o brabo",
+    })
+  );
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import * as http from "http";
+import { getListEpisodes } from "./controllers/podcasts-controller";
 
 const server = http.createServer(
-  (request: http.IncomingMessage, response: http.ServerResponse) => {}
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.method === "GET") {
+      await getListEpisodes(req, res);
+    }
+  }
 );
 
 const port = process.env.PORT;
