d7833494c13fbcb54e4d66d98d2d473426f11d2e
Thallentos <thallescmarques@gmail.com>
2025-07-03 05:56:05 -0300

    criando o servidor local, a arquitetura em forma dos diretórios e o controller

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import * as http from "http";
+
+const server = http.createServer(
+  (request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Servidor iniciado na porta ${port}`);
+});
